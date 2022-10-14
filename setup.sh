dab4e45b2c419dc798ecfe27e9dc6b77e5780840
Ankur Singh <as.ankursingh3.1@gmail.com>
2022-10-14 13:12:37 -0700

    added setup.sh

---
+++ setup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+nvidia_containe_installed=$(apt list nvidia-container --installed 2> /dev/null | wc -l)
+if [ ${nvidia_containe_installed} = 1 ]; then
+    echo "nvidia-container is not installed. Installing it now..."
+    apt update && apt install -y nvidia-container
+fi
+tee /etc/docker/daemon.json << EOF
+{
+    "runtimes": {
+        "nvidia": {
+            "path": "nvidia-container-runtime",
+            "runtimeArgs": []
+        }
+    },
+    "default-runtime": "nvidia"
+}
+EOF
+systemctl restart docker
